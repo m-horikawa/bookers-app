6aeb8d92b2daf3acc7b24882bd0db234a8699224
EC2 Default User <ec2-user@ip-172-31-21-60.ec2.internal>
2021-02-06 10:21:28 +0000

    Second Commit

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -3,45 +3,40 @@ class BooksController < ApplicationController
     # Viewへ渡すためのインスタンス変数に空のモデルオブジェクトを生成する。
      @list = List.new
   end
-
   def create
-    # １. データを新規登録するためのインスタンス作成
     list = List.new(list_params)
-    # ２. データをデータベースに保存するためのsaveメソッド実行
-    list.save
-    # redirect_to '/top' を削除して、以下コードに変更
-    # 詳細画面へリダイレクト
-    redirect_to book_path(list.id)
+    if list.save
+      redirect_to book_path(list.id), notice: 'Book was successfully created.'
+    else
+      #書き込み失敗
+      render action: :new
+    end
   end
-
   def index
      @lists = List.all
   end
-
   def show
     @list = List.find(params[:id])
   end
-
   def edit
     @list = List.find(params[:id])
   end
-
   def update
     list = List.find(params[:id])
-    list.update(list_params)
-    redirect_to book_path(list.id)
+    if list.update(list_params)
+      redirect_to book_path(list.id)
+    else
+      render action: :edit
+    end
   end
-
   def destroy
     list = List.find(params[:id])  # データ（レコード）を1件取得
     list.destroy  # データ（レコード）を削除
     redirect_to books_path  # 投稿一覧画面へリダイレクト
   end
-
   private
   # ストロングパラメータ
   def list_params
     params.require(:list).permit(:title, :body)
   end
-
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,10 @@
 Rails.application.routes.draw do
-  get 'books/new'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get 'top' => 'homes#top'
+  get '/' => 'homes#top'
+  get 'books' => 'books#index', as: 'book_index'
   post 'books' => 'books#create'
-  get 'books' => 'books#index'
-   # .../todolists/1 や .../todolists/3 に該当する
-  get 'books/:id' => 'books#show', as: 'book'
+  get 'books/new' => 'books#new', as: 'new_book'
   get 'books/:id/edit' => 'books#edit', as: 'edit_book'
+  get 'books/:id' => 'books#show', as: 'book'
   patch 'books/:id' => 'books#update', as: 'update_book'
   delete 'books/:id' => 'books#destroy', as: 'destroy_book'
 end
